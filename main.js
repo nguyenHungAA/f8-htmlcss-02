2ed2ed682746487800ee96a0396a7bb0f5ba1492
unknown <you@example.com>
2024-05-07 09:06:22 +0700

    Hero

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log(document.getElementById("heading"));
+console.log(document.getElementsByClassName('header'));
+console.log(document.querySelector('.header'));
+console.log(document.anchors);
+
+var boxNode = document.querySelector('heading');
+
+console.log(boxNode.querySelectorAll('p')); // tim tu con cua element da lay ra truoc do
